6e81449fa336ce728661ab8b85460e1557200e1a
Batyr Atayev <atayev2012@gmail.com>
2024-09-22 21:04:52 +0300

    add encircled user photo to interface

--- task 2 - User Profile/UserProfile/UserProfile/MainViewController.swift
+++ task 2 - User Profile/UserProfile/UserProfile/MainViewController.swift
@@ -11,13 +11,37 @@ class MainViewController: UIViewController{
     
     
     private var headerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+        let headView = UIView()
+        headView.backgroundColor = .red
+        headView.translatesAutoresizingMaskIntoConstraints = false
+        return headView
         
     }()
     
+    private var editPhotoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Edit", for: .normal)
+        button.currentImage = (image: UIImage(systemItem: .trash), for: <#T##UIControl.State#>)
+        
+        return button
+    }()
+    
+    private var profileImageView: UIImageView = {
+        let userImageView  = UIImageView()
+        userImageView.image = UIImage(named: "userIMG") // set image from variables loaded from csv file
+        
+        userImageView.layer.borderWidth = 2
+        userImageView.layer.borderColor = UIColor.white.cgColor
+        
+        userImageView.layer.cornerRadius = 75.0
+        userImageView.layer.masksToBounds = true
+        
+        userImageView.contentMode = .scaleAspectFit
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return userImageView
+    }()
     
     
     
@@ -31,13 +55,20 @@ class MainViewController: UIViewController{
         view.backgroundColor = .white
         
         view.addSubview(headerView)
+        view.addSubview(profileImageView)
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
             headerView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
             headerView.widthAnchor.constraint(equalToConstant: view.frame.size.width),
-            headerView.heightAnchor.constraint(equalToConstant: view.status)
+            headerView.heightAnchor.constraint(equalToConstant: 200),
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            profileImageView.centerYAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 0.0),
+            profileImageView.widthAnchor.constraint(equalToConstant: 150),
+            profileImageView.heightAnchor.constraint(equalToConstant: 150)
         ])
+        
+        
         
     }
 }
